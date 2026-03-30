9c70897ed60464fb51f21101b60e471bb9af441a
durgaprasadkurma2015 <durgaprasadkurma2015@gmail.com>
2026-03-30 21:45:14 +0530

    if else

---
+++ src/Control_Statement/ifElse.java
@@ -0,0 +1,17 @@
+package Control_Statement;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		
+		if(x>=5) {
+			System.out.println("x=5");
+		}
+			else {
+				System.err.println("error");
+			}
+		}
+	
+}
